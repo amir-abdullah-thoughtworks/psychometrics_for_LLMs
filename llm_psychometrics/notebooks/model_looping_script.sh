cdd2ca1a6bcedf52643d06dacadf503778c97347
Shreyans <jshrey8@gmail.com>
2025-09-29 11:25:54 -0600

    ran reliability experiments for some base models

--- llm_psychometrics/notebooks/model_looping_script.sh
+++ llm_psychometrics/notebooks/model_looping_script.sh
@@ -1,20 +1,22 @@
-models=("gpt-4.1-mini" "meta-llama/Llama-3.2-1B-Instruct" "Qwen/Qwen3-0.6B")
+models=( "Qwen/Qwen3-4B"
+"Qwen/Qwen3-8B" "gpt-4.1-mini" "meta-llama/Llama-3.2-1B-Instruct" "Qwen/Qwen3-0.6B" "meta-llama/Llama-3.2-3B-Instruct" "gpt-4.1" )
 
-# "meta-llama/Llama-3.2-3B-Instruct" "gpt-4.1" 
-# "meta-llama/Llama-3.1-8B-Instruct"  "Qwen/Qwen3-1.7B" "Qwen/Qwen3-4B"
-# "Qwen/Qwen3-8B")
+# Access pending: "meta-llama/Llama-3.1-8B-Instruct"
+
+# "Qwen/Qwen3-1.7B"
 
 # Command for Experiment 1 - Base Model
-for model in "${models[@]}"; do
-    echo "Running for $model"
-    python3 hf_personas_hexaco_v0.py --model-name $model \
-    --persona-source="base_model"
-done
-
-
-# # Command for Experiment 1 - Simple Personas
 # for model in "${models[@]}"; do
 #     echo "Running for $model"
 #     python3 hf_personas_hexaco_v0.py --model-name $model \
-#     --persona-source="personallm_paper"
+#     --persona-source="base_model" \
+#     --hf-token="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"
 # done
+
+
+# Command for Experiment 1 - Simple Personas
+for model in "${models[@]}"; do
+    echo "Running for $model"
+    python3 hf_personas_hexaco_v0.py --model-name $model \
+    --persona-source="personallm_paper"
+done
