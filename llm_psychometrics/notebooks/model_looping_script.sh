669106da84ce1df15eb731d6516cda23be6eadb7
Shreyans <jshrey8@gmail.com>
2025-09-29 13:25:10 -0600

    switched qwen3 models with qwen2.5 instruct variants

--- llm_psychometrics/notebooks/model_looping_script.sh
+++ llm_psychometrics/notebooks/model_looping_script.sh
@@ -1,20 +1,20 @@
-models=("Qwen/Qwen3-4B"
-"Qwen/Qwen3-8B" "gpt-4.1-mini" "meta-llama/Llama-3.2-1B-Instruct" "Qwen/Qwen3-0.6B" "meta-llama/Llama-3.2-3B-Instruct" "gpt-4.1" "meta-llama/Llama-3.1-8B-Instruct")
+models=("Qwen/Qwen2.5-0.5B-Instruct" "Qwen/Qwen2.5-7B-Instruct"
+"Qwen/Qwen2.5-1.5B-Instruct" "Qwen/Qwen2.5-3B-Instruct")
 
-# "Qwen/Qwen3-1.7B"
+# "gpt-4.1-mini" "gpt-4.1" "meta-llama/Llama-3.2-1B-Instruct"  "meta-llama/Llama-3.2-3B-Instruct"  "meta-llama/Llama-3.1-8B-Instruct"
 
 # Command for Experiment 1 - Base Model
-# for model in "${models[@]}"; do
-#     echo "Running for $model"
-#     python3 hf_personas_hexaco_v0.py --model-name $model \
-#     --persona-source="base_model" \
-#     --hf-token="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"
-# done
-
-
-# Command for Experiment 1 - Simple Personas
 for model in "${models[@]}"; do
     echo "Running for $model"
     python3 hf_personas_hexaco_v0.py --model-name $model \
-    --persona-source="personallm_paper"
+    --persona-source="base_model" \
+    --hf-token="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"
 done
+
+
+# Command for Experiment 1 - Simple Personas
+# for model in "${models[@]}"; do
+#     echo "Running for $model"
+#     python3 hf_personas_hexaco_v0.py --model-name $model \
+#     --persona-source="personallm_paper"
+# done
