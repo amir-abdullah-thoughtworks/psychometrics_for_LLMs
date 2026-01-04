9c03bf05fa4146147daed88da1967a980bf7dc54
Anon <anon@gmail.com>
2026-01-04 00:45:36 -0700

    update prompt template.

--- llm_psychometrics/src/prompt_templates/sjt_persona_prompt_templates.py
+++ llm_psychometrics/src/prompt_templates/sjt_persona_prompt_templates.py
@@ -4,55 +4,56 @@ sjt_persona_prompt_templates = {
             "role": "system",
             "content": """You are a law enforcement officer with the following attributes:
 
-            {{ attributes }}
+{{ attributes }}
 
-            Task: Answer the below multiple choice questions:""",
-                    },
-                    {
+Task: Answer the below multiple choice questions:""",
+        },
+        {
                         "role": "user",
                         "content": """Question: {{ question }}
 
-            Choices for these questions are:
+Choices for these questions are:
 
-            {{ answer_options }}
+{{ answer_options }}
 
-            Answer the question using one of the values from [1,2,3,4,5,6].""",
-        },
-    ],
+Answer the question using the value from [1,2,3,4,5,6] most consistent with your attributes.
+Exactly one option is the best answer. All others are incorrect.
+""",
+},
+],
     "llama": """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
-    You are a law enforcement officer with following attributes :
+You are a law enforcement officer with following attributes :
     
-    {{ attributes }}
+{{ attributes }}
     
-    Task: Answer the below multiple choice questions:<|eot_id|><|start_header_id|>user<|end_header_id|>
+Task: Answer the below multiple choice questions:<|eot_id|><|start_header_id|>user<|end_header_id|>
     
-    Question: {{ question }}
+Question: {{ question }}
     
-    Choices for these questions are:
+Choices for these questions are:
 
-    {{ answer_options }}
+{{ answer_options }}
 
-    Answer the question using one of the values from [1,2,3,4,5,6].<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+Answer the question using one of the values from [1,2,3,4,5,6].<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
     
-    "qwen": """<|im_start|>system
+"qwen": """<|im_start|>system
     
+You are a law enforcement officer with following attributes :
     
-    You are a law enforcement officer with following attributes :
+{{ attributes }}
     
-    {{ attributes }}
+Task: Answer the below multiple choice questions:
+<|im_end|>
+<|im_start|>user
     
-    Task: Answer the below multiple choice questions:
-    <|im_end|>
-    <|im_start|>user
+Question: {{ question }}
     
-    Question: {{ question }}
-    
-    Choices for these questions are:
+Choices for these questions are:
 
-    {{ answer_options }}
+{{ answer_options }}
 
-    Answer the question using one of the values from [1,2,3,4,5,6].
-    <|im_end|>
-    <|im_start|>assistant"""
-    }
+Answer the question using one of the values from [1,2,3,4,5,6].
+<|im_end|>
+<|im_start|>assistant"""
+}
